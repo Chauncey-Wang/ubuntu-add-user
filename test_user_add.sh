7e7e3d7928048803d9cc83f01e76510139a27971
Chauncey-Wang <37097826+Chauncey-Wang@users.noreply.github.com>
2020-12-29 16:42:15 +0800

    Add files via upload

---
+++ test_user_add.sh
@@ -0,0 +1,16 @@
+for username in $(more users.list)
+do
+	if [ -n $username ]
+	then
+		useradd -d /home/$username -s /bin/bash -m $username
+		echo
+		# echo $ username"123" | paswd --stdin $username
+		# echo $ username":"'123456' | chpasswd
+		echo "$username:123456" | chpasswd
+		echo
+		echo "User $username's password is changed!"
+	else
+		echo "The username is null!"
+	fi
+done
+
